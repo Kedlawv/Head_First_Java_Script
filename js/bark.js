20aa93e42357bc8a4764a1bc7a1e56339442cd66
Kedlawv <waldek.bus@gmail.com>
2020-02-19 23:16:29 +0100

    function bark

---
+++ js/bark.js
@@ -0,0 +1,10 @@
+function bark(weight, name){
+    if(weight > 20){
+        console.log(name + " goes Woof Woof");
+    }else{
+        console.log(name + " goes woof woof");
+    }
+}
+
+bark(16,"Ciapek");
+bark(30,"Behemot");
