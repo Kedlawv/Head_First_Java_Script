c7b212544ab62c59522121b1970134ea1691a163
Kedlawv <waldek.bus@gmail.com>
2020-02-27 11:16:29 +0100

    Controller object processGuess

--- battleships-2/battleships.js
+++ battleships-2/battleships.js
@@ -23,20 +23,20 @@ var model = {
     shipsSunk: 0,
 
     ships: [
-        {locations: ["06","16","26"], hits: ["","",""]},
-        {locations: ["24","34","44"], hits: ["","",""]},
-        {locations: ["10","11","12"], hits: ["","",""]}
+        {locations: ["06", "16", "26"], hits: ["", "", ""]},
+        {locations: ["24", "34", "44"], hits: ["", "", ""]},
+        {locations: ["10", "11", "12"], hits: ["", "", ""]}
     ],
 
-    fire: function(guess){
+    fire: function (guess) {
         for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
             let index = ship.locations.indexOf(guess);
-            if(index >= 0){
+            if (index >= 0) {
                 ship.hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("HIT!");
-                if(this.isSunk(ship)){
+                if (this.isSunk(ship)) {
                     view.displayMessage("You sank my battleship! Damn you!!!");
                     this.shipsSunk++;
                 }
@@ -48,9 +48,9 @@ var model = {
         return false;
     },
 
-    isSunk: function(ship){
+    isSunk: function (ship) {
         for (let i = 0; i < this.shipLength; i++) {
-            if(ship.hits[i] !== "hit"){
+            if (ship.hits[i] !== "hit") {
                 return false;
             }
         }
@@ -61,27 +61,36 @@ var model = {
 let controller = {
     guesses: 0,
 
-    processGuess : function(guess){
+    processGuess: function (guess) {
+        let location = this.parseGuess(guess);
 
+        if (location) {
+            this.guesses++
+            let hit = model.fire(guess);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("Well done Cap! You sunk all of my ships in "
+                    + this.guesses + " guesses.")
+            }
+        }
     },
 
-    parseGuess: function(guess){
-        let alphabet = ["A","B","C","D","E","F","G"];
+    parseGuess: function (guess) {
+        let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
-        if(guess === null || guess.length !== 2){
+        if (guess === null || guess.length !== 2) {
             alert("Sorry wrong input, please enter a letter and a number corresponding to the cell " +
                 "on the bord.")
-        }else{
+        } else {
             let firstChar = guess.charAt(0);
             let row = alphabet.indexOf(firstChar);
             let column = guess.charAt(1);
 
-            if(isNaN(row) || isNaN(column)){
+            if (isNaN(row) || isNaN(column)) {
                 alert("Oops, that is not on the board");
-            }else if(row < 0 || column < 0
-            || row >= model.boardSize || column >= model.boardSize){
+            } else if (row < 0 || column < 0
+                || row >= model.boardSize || column >= model.boardSize) {
                 alert("Nice shooting matey, that of the board!")
-            }else{
+            } else {
                 return row + column;
             }
         }
