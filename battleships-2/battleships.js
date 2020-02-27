c12085feb1be430fb5cfb900efa873cf1753382f
Kedlawv <waldek.bus@gmail.com>
2020-02-27 08:25:30 +0100

    Model object for keeping state of the game

--- battleships-2/battleships.js
+++ battleships-2/battleships.js
@@ -1,26 +1,61 @@
 let view = {
-    displayMessage: function(msg){
+    displayMessage: function (msg) {
         let messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
 
     displayHit: function (location) {
         let cell = document.getElementById(location);
-        cell.setAttribute("class","hit");
+        cell.setAttribute("class", "hit");
     },
 
     displayMiss: function (location) {
         let cell = document.getElementById(location);
-        cell.setAttribute("class","miss");
+        cell.setAttribute("class", "miss");
     }
 
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+var model = {
+    board: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
 
-view.displayMessage("Tap tap, is this thing on?");
+    ships: [
+        {locations: ["06","16","26"], hits: ["","",""]},
+        {locations: ["24","34","44"], hits: ["","",""]},
+        {locations: ["10","11","12"], hits: ["","",""]}
+    ],
+
+    fire: function(guess){
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if(index >= 0){
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if(this.isSunk(ship)){
+                    view.displayMessage("You sank my battleship! Damn you!!!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("Miss! Ha!");
+        return false;
+    },
+
+    isSunk: function(ship){
+        for (let i = 0; i < this.shipLength; i++) {
+            if(ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
