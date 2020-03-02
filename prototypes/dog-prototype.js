6a50285b0f50df3681b0f43b2a549b6cd7b9c955
Kedlawv <waldek.bus@gmail.com>
2020-03-02 15:29:24 +0100

    change => to function expression | we cannot use => anywhere we need 'this' as in this example it cannot be used in the prototype method. 'this' was pointing to the window object instead of the object calling the method when => was used

--- prototypes/dog-prototype.js
+++ prototypes/dog-prototype.js
@@ -5,15 +5,15 @@ function Dog(name, breed, weight) { // first we need a constructor
 }
 
 Dog.prototype.species = "Canine";   // then we add properties to the prototype property object of the
-Dog.prototype.bark = () => {        // constructor
+Dog.prototype.bark = function() {        // constructor
     if (this.weight > 25) {
-        return "Woof";
+        return this.name + " says Woof!";
     } else {
-        return "Yip";
+        return this.name + " says Yip!";
     }
 };
-Dog.prototype.wag = () => {
-    return "Wag!";
+Dog.prototype.wag = () => { // big difference between function expression and => operator
+    return "Wag!";          // => does not set 'this' to the object calling the function
 };
 Dog.prototype.run = () => {
     return "Run!"
@@ -26,13 +26,15 @@ function init(){
     let fluffy = new Dog("Fluffy", "Poodle", 30);
     let spot = new Dog("Spot", "Chihuahua", 10);
 
-    spot.bark = ()=>{       // we can override the method, javascript always looks in the local scope
-        return "WOOF!!!!"   // first and if it doesn't find the method it look in the prototype
+    spot.bark = function(){    // we can override the method, javascript always looks in the local scope
+        return this.name + " says WOOF!!!!" // first and if it doesn't find the method it look in
+                                            // the prototype
     };
 
     display.innerHTML += fido.run() + "</br>";
     display.innerHTML += fido.wag() + "</br>";
     display.innerHTML += fido.bark() + "</br></br>";
+
 
     display.innerHTML += fluffy.run() + "</br>";
     display.innerHTML += fluffy.wag() + "</br>";
